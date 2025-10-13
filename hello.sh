7244de2f686ddcdad926f07d2b67e39cf726a82a
ahmedmahmoudabuzaidd <127856378+ahmedmahmoudabuzaidd@users.noreply.github.com>
2025-10-13 16:22:03 +0300

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,8 @@
 #! /bin/bash
-echo "Hello from Script"
+if [ "$(sudo systemctl is-active httpd)" == "active" ]  ; then
+        echo "Service is running"
+else
+        sudo systemctl start httpd
+        sudo systemctl enable httpd
+        echo "service is started and enabled"
+fi
